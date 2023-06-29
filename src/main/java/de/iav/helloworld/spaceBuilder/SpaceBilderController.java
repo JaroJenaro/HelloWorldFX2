b006162dde3630a3bf80de19e89fbb34a6c15a6a
JaroJenaro <jaroslaw.placzek.job@icloud.com>
2023-06-29 14:17:25 +0200

    clearForm() implementiert 2

--- src/main/java/de/iav/helloworld/spaceBuilder/SpaceBilderController.java
+++ src/main/java/de/iav/helloworld/spaceBuilder/SpaceBilderController.java
@@ -1,11 +1,17 @@
 package de.iav.helloworld.spaceBuilder;
 
 import de.iav.helloworld.model.Student;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.util.Duration;
 
 public class SpaceBilderController {
     @FXML
@@ -16,6 +22,14 @@ public class SpaceBilderController {
     public TextField e_email;
 
     @FXML
+    public TextField tf_showInLabel;
+    @FXML
+    public Label l_viewTextField;
+    @FXML
+    public Label l_errorMessage;
+
+
+    @FXML
     private Label welcomeText;
     @FXML
     private Button b_courseUpdate;
@@ -24,6 +38,53 @@ public class SpaceBilderController {
     @FXML
     private TextField e_lastName;
 
+/*
+    public void initialize() {
+        System.out.println("This method is executed on initialization of the controller");
+        //label.textProperty().bind(textField.textProperty());
+        // oder
+        tf_showInLabel.textProperty().addListener((observable, oldValue, newValue) -> {
+            if(newValue.length()>=3) {
+                l_viewTextField.setText(newValue);
+            }
+            else {l_viewTextField.setText("Länge 3 nicht erreicht");
+
+            }
+        });
+    }
+*/
+    public void initialize() {
+        System.out.println("This method is executed on initialization of the controller");
+        //label.textProperty().bind(textField.textProperty());
+        // oder
+        tf_showInLabel.textProperty().addListener((observable, oldValue, newValue) -> {
+            if(newValue.length()>=3) {
+                System.out.println("if newValue: " +  newValue + "neValue-length() " +newValue.length() + " oldValue: " +  oldValue);
+                //l_viewTextField.setText(newValue);
+                //label.setText("Started");
+                final IntegerProperty i = new SimpleIntegerProperty(0);
+                Timeline timeline = new Timeline(
+                        new KeyFrame(
+                                Duration.seconds(3),
+                                event -> {
+                                    i.set(i.get() + 1);
+                                    l_viewTextField.setText(newValue);
+                                    l_errorMessage.setText("");
+                                    welcomeText.setText("Elapsed time: " + i.get() + " seconds");
+                                }
+                        )
+                );
+                timeline.setCycleCount(Animation.INDEFINITE);
+                timeline.play();
+            }
+            else {
+                System.out.println("else newValue: " +  newValue + " oldValue: " +  oldValue);
+                l_errorMessage.setText("Länge 3 nicht erreicht");
+                l_viewTextField.setText("");
+
+            }
+        });
+    }
 
     public void curseUpdate() {
         lv_ListView.getItems().add(comboBox.getValue());
@@ -43,6 +104,9 @@ public class SpaceBilderController {
         e_lastName.clear();
         e_email.clear();
         lv_ListView.getItems().clear();
+        tf_showInLabel.clear();
+        l_viewTextField.setText("");
+
 
     }
 }
